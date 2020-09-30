a914c71576923a642cedd072dd2b7142beb8745a
Wozeo <36730713+Wozeo@users.noreply.github.com>
2020-09-30 15:45:19 +0200

    Exemple

--- CeciEstUnTest/src/CeciEstUnSuperTest.java
+++ CeciEstUnTest/src/CeciEstUnSuperTest.java
@@ -1,11 +1,36 @@
 public class CeciEstUnSuperTest {
 
+
+
     public static void main(String[] args) {
         System.out.println("Ceci est un affichage de test");
         nbCpl z = new nbCpl(3,5);
         System.out.println(z.norme());
         compterDeAaB(1,10,false);
         System.out.println(exampleIf(1,100));
+        testTableau();
+    }
+
+
+
+
+    static void testTableau(){
+        float t0[] = {0,10,1390};
+        float t1[] = new float[200];
+        float t2[][];
+
+        System.out.println(t0[1]);
+        t0 = new float[]{1,2,10};
+        System.out.println(t0[1]);
+
+        System.out.println(t1[0]);
+        for(int i = 0; i < 200 ; i ++){
+            t1[i] = i*i;
+        }
+        System.out.println(t1[7]);
+
+        t2 = new float[][]{{1,2,3},{7,8,9},{11,12,13}};
+        System.out.println(t2[2][1]);
     }
 
     static class nbCpl{
